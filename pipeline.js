b6bf283a563406183a8c37f73bd980de5dc86b0b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-06-21 23:57:36 -0400

    Working join(samples, fastqDump) which properly resolves inputs

--- pipeline.js
+++ pipeline.js
@@ -4,26 +4,45 @@
 const { spawn } = require('child_process')
 const globby = require('globby')
 
-const task = (props, cb) => (next) => {
-// const task = (props, cb, next) => {
-  const { output } = props
-
-  const stream = cb(props)
-
-  // Check current dir for files matching the pattern provided to the task
-  // Then call next task in the pipeline with the resolved files
-  stream.on('end', () => globby(output).then(next))
-
-  return stream
+// mostly for instanceof checks inside task
+class File {
+  constructor(value) {
+    this.value = value
+  }
 }
 
-// function join(...tasks) {
-//   tasks.map((t, i) => {
-//     if (i !== tasks.length-1) {
-//       return task()
-//     }
-//   })
-// }
+const task = (props, cb) => (next) => () => {
+  let { input, output } = props
+
+  // TODO recursively traverse object
+  // TODO use generators to yield Promises to be cleaner
+  if (input instanceof File) {
+    // If we got special classes as input, they are either stdin or files,
+    // so we need to resolve the pattern
+    globby(input.value)
+      .then((data) => {
+        props.input = data[0]
+        return ready()
+      })
+  } else {
+    return ready()
+  }
+
+  function ready() {
+    const stream = cb(props)
+
+    // Check current dir for files matching the pattern provided to the task
+    // Then call next task in the pipeline with the resolved files
+    stream.on('end', () => globby(output).then(next))
+
+    return stream
+  }
+}
+
+function join(...tasks) {
+  // return tasks[0](tasks[1]())
+  return tasks.reduce( (prev, curr) => prev(curr()) )
+}
 
 const shell = (cmd) => {
   console.log('Starting: ' + cmd)
@@ -31,17 +50,11 @@ const shell = (cmd) => {
 
   const process = spawn(cmd[0], cmd.slice(1))
 
-  process.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`)
-  })
+  process.stdout.on('data', (data) => console.log(`stdout: ${data}`) )
 
-  process.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`)
-  })
+  process.stderr.on('data', (data) => console.log(`stderr: ${data}`) )
 
-  process.on('close', (code) => {
-    console.log(`child process exited with code ${code}`)
-  })
+  process.on('close', (code) => console.log(`child process exited with code ${code}`) )
 }
 
 // === PIPELINE ===
@@ -61,8 +74,8 @@ const config = {
 
 // const samples = task(...)
 // const samples = () => task(...)
-function samples() {
-  // task(props, cb, next)
+function samples(next) {
+  // task(props, cb)(next)
   return task(
     // these params are then made available to the cb
     {
@@ -75,28 +88,25 @@ function samples() {
     },
     // the cb for this task
     ( ({ input }) => ncbi.download(input.db, input.accession) )
-
-    // the next task for this task is curried
-    // data is the resolved output
-  )( (data) => shell(`fastq-dump --split-files --skip-technical --gzip ${data[0]}`) )
-  // can do () to have next be undefined in the task manager and it will end
+  )(next)
 }
 
-// wip
-// function dumps() {
-//   return task({
-//     input: '*.sra',
-//     // input: stdin('*.sra')
-//     output: [1, 2].map(n => `*_${n}.fastq.gz`)
-//   }, ( { input }) => {
-//     console.log(input)
-//     // shell(`fastq-dump --split-files --skip-technical --gzip ${input}`)
-//   })
-// }
+function fastqDump() {
+  return task({
+    input: new File('**/*.sra'),
+    output: [1, 2].map(n => `*_${n}.fastq.gz`)
+  },
+  ({ input }) => shell(`fastq-dump --split-files --skip-technical --gzip ${input}`)
+  )()
+}
 
-// join(samples, dumps)
+// pass in next which is given globbied output
+// const pipeline = samples( (data) => shell(`fastq-dump --split-files --skip-technical --gzip ${data[0]}`) )
+// or nothing to stop the pipeline
+// const pipeline = samples()
 
-const pipeline = samples
+// join will populate the nexts
+const pipeline = join(samples, fastqDump)
 
 pipeline()
   .on('data', console.log)
