4c793123c1363433c914056843270c78649782c5
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-06-23 19:33:49 -0400

    Remove unnecessary function wrappers in pipeline for tasks

--- pipeline.js
+++ pipeline.js
@@ -6,12 +6,6 @@ const { task, join, parallel } = waterwheel
 const { File } = waterwheel.types
 const { shell, shellPipe } = waterwheel.wrappers
 
-// === UTILS ===
-const last = (str, sep) => {
-  const splitted = str.split(sep)
-  return splitted[splitted.length - 1]
-}
-
 // === PIPELINE ===
 const fs = require('fs')
 const ncbi = require('bionode-ncbi')
@@ -23,115 +17,62 @@ const config = {
   referenceURL: 'http://ftp.ncbi.nlm.nih.gov/genomes/all/GCA_000988525.2_ASM98852v2/GCA_000988525.2_ASM98852v2_genomic.fna.gz'
 }
 
-// const samples = task(...)
-// const samples = () => task(...)
-function samples(next) {
-  // task(props, cb)(next)
-  return task(
-    // these params are then made available to the cb
-    {
-      input: {
-        db: 'sra',
-        accession: config.sraAccession
-      },
-      // will be globby'ed after cb completes
-      output: '**/*.sra'
-    },
-    // the cb for this task
-    ( ({ input }) => ncbi.download(input.db, input.accession) )
-  )(next)
-}
+// task(props, cb)
+// cb(props) will be called after Files in props are resolved with globby
+// cb is the "task action creator" and should return a Stream, Promise, or callback
 
-function fastqDump(next) {
-  return task(
-  {
-    input: new File('**/*.sra'),
-    output: [1, 2].map(n => new File(`*_${n}.fastq.gz`))
+const samples = task({
+  input: {
+    db: 'sra',
+    accession: config.sraAccession
   },
-  ({ input }) => shell(`fastq-dump --split-files --skip-technical --gzip ${input}`)
-)(next)
-}
+  output: '**/*.sra'
+}, ({ input }) => ncbi.download(input.db, input.accession) )
 
-function downloadReference(next) {
-  return task(
-  {
-    input: config.referenceURL,
-    output: new File(last(config.referenceURL, '/'))
-  },
-  ({ input, output }) => request(input).pipe(fs.createWriteStream(output.value))
-  )(next)
-}
+const fastqDump = task({
+  input: new File('**/*.sra'),
+  output: [1, 2].map(n => new File(`*_${n}.fastq.gz`))
+}, ({ input }) => shell(`fastq-dump --split-files --skip-technical --gzip ${input}`) )
 
-function bwaIndex(next) {
-  return task(
-  {
-    input: new File('*_genomic.fna.gz'),
-    output: ['amb', 'ann', 'bwt', 'pac', 'sa'].map(suffix => new File(`*_genomic.fna.gz.${suffix}`))
-  },
-  ({ input }) => shell(`bwa index ${input}`)
-  )(next)
-}
+const downloadReference = task({
+  input: config.referenceURL,
+  output: new File(config.referenceURL.split('/').pop())
+}, ({ input, output }) => request(input).pipe(fs.createWriteStream(output.value)) )
 
-function alignAndSort(next) {
-  return task(
-  {
-    input: [new File('*_genomic.fna.gz'), new File('*.fastq.gz')],
-    output: new File('reads.bam')
-  },
-  ({ input }) => shellPipe([
-    `bwa mem -t ${THREADS} ${input[0]} ${input[1]} ${input[2]}`,
-    'samtools view -Sbh -',
-    'samtools sort - -o reads.bam'
-  ])
-  )(next)
-}
+const bwaIndex = task({
+  input: new File('*_genomic.fna.gz'),
+  output: ['amb', 'ann', 'bwt', 'pac', 'sa'].map(suffix => new File(`*_genomic.fna.gz.${suffix}`))
+}, ({ input }) => shell(`bwa index ${input}`) )
 
-function samtoolsIndex(next) {
-  return task(
-  {
-    input: new File('*.bam'),
-    output: new File('*.bam.bai')
-  },
-  ({ input }) => shell(`samtools index ${input}`)
-  )(next)
-}
+const alignAndSort = task({
+  input: [new File('*_genomic.fna.gz'), new File('*.fastq.gz')],
+  output: new File('reads.bam')
+}, ({ input }) => shellPipe([
+  `bwa mem -t ${THREADS} ${input[0]} ${input[1]} ${input[2]}`,
+  'samtools view -Sbh -',
+  'samtools sort - -o reads.bam'
+]) )
 
-function decompressReference(next) {
-  return task(
-  {
-    input: new File('*_genomic.fna.gz'),
-    output: new File('*_genomic.fna')
-  },
-  ({ input }) => shell(`bgzip -d ${input}`)
-  )(next)
-}
+const samtoolsIndex = task({
+  input: new File('*.bam'),
+  output: new File('*.bam.bai')
+}, ({ input }) => shell(`samtools index ${input}`) )
 
-function mpileupAndCall(next) {
-  return task(
-  {
-    input: [new File('*_genomic.fna'), new File('*.bam'), new File('*.bam.bai')],
-    output: new File('variants.vcf')
-  },
-  ({ input }) => shellPipe([
-    `samtools mpileup -uf ${input[0]} ${input[1]}`,
-    'bcftools call -c - > variants.vcf'
-  ])
-  )(next)
-}
+const decompressReference = task({
+  input: new File('*_genomic.fna.gz'),
+  output: new File('*_genomic.fna')
+}, ({ input }) => shell(`bgzip -d ${input}`) )
 
-// function after(next) {
-//   return task(
-//   {
-//     input: null,
-//     output: null
-//   },
-//   () => shell('echo AFTER')
-//   )(next)
-// }
+const mpileupAndCall = task({
+  input: [new File('*_genomic.fna'), new File('*.bam'), new File('*.bam.bai')],
+  output: new File('variants.vcf')
+}, ({ input }) => shellPipe([
+  `samtools mpileup -uf ${input[0]} ${input[1]}`,
+  'bcftools call -c - > variants.vcf'
+]) )
 
 const pipeline = parallel({
   taskLists: [[samples], [downloadReference, bwaIndex]],
-  // next: alignAndSort
   next: join(alignAndSort, samtoolsIndex, decompressReference, mpileupAndCall)
 })
 pipeline()
