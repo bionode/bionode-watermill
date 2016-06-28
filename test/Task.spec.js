ae594984b5738a19c3e8606e89b8e927fef84202
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-06-28 15:32:56 -0400

    ReadableFromPromise instead of duplex

--- test/Task.spec.js
+++ test/Task.spec.js
@@ -1,36 +1,28 @@
-const { assert } = require('chai')
+const { assert, expect, should } = require('chai')
+const streamAssert = require('stream-assert')
 const isStream = require('isstream')
+const { isReadable, isWritable, isDuplex } = isStream
 
 const Task = require('../lib/Task.js')
 
+const noop = () => {}
+const fs = require('fs')
+
 describe('Task', function() {
-  // describe('checking parameters', function(done) {
-  //   it('should error if not provided with a function', function(done) {
-  //     done(new Error('TODO'))
-  //   })
-  // })
+  describe('checking parameters', function(done) {
+    it.skip('should error if not provided with a function', function() {
 
-  describe('provided with a Promise', function() {
-    it('should return a stream', function(done) {
-      const task = Task(
-        {
-          input: 100
-        },
-        ({ input }) => new Promise((resolve, reject) => {
-          setTimeout(() => resolve('DATUMS'), input)
-        })
-      )()
+    })
+  })
 
-      task
-        .on('data', (data) => {
-          console.log('data: ' + data)
-          done()
-        })
-        .on('end', (data) => console.log(data))
+  describe('when provided with a Promise', function() {
+    describe('that resolves to a value', function() {
+      it('should return a readable stream', function() {
+        const task = Task(null, () => new Promise((resolve, reject) => resolve('datums')))
 
-      assert(isStream(task))
-
-      // done(new Error('TODO'))
+        assert.isOk(isReadable(task))
+        assert.isNotOk(isWritable(task))
+      })
     })
   })
 })
