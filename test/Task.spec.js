65627d72f58b9e0126340a3c614c2a8407ad5c3e
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-06-28 17:42:42 -0400

    task can take and return read/write/duplex streams

--- test/Task.spec.js
+++ test/Task.spec.js
@@ -2,11 +2,15 @@ const { assert, expect, should } = require('chai')
 const streamAssert = require('stream-assert')
 const isStream = require('isstream')
 const { isReadable, isWritable, isDuplex } = isStream
+const intoStream = require('into-stream')
+const through = require('through2')
+const duplexify = require('duplexify')
 
 const Task = require('../lib/Task.js')
 
 const noop = () => {}
 const fs = require('fs')
+const path = require('path')
 
 describe('Task', function() {
   describe('checking parameters', function(done) {
@@ -40,6 +44,52 @@ describe('Task', function() {
       task
         .on('data', noop)
         .on('end', () => done())
+    })
+  })
+
+  describe('when provided with a readable stream', function() {
+    it('should return a readable stream', function(done) {
+      const task = Task(null, () => intoStream('unicorn'))
+
+      assert.isOk(isReadable(task))
+      assert.isNotOk(isWritable(task))
+
+      task
+        .on('data', noop)
+        .on('end', () => done())
+    })
+  })
+
+  describe('when provided with a writable stream', function() {
+    it('should return a writable stream', function(done) {
+      const task = Task(null, () => fs.createWriteStream(path.resolve(__dirname, 'writable.log')))
+
+      task.write('one\n')
+      task.write('two\n')
+      task.end()
+
+      assert.isOk(isWritable(task))
+      assert.isNotOk(isReadable(task))
+
+      task
+        .on('finish', () => done())
+    })
+  })
+
+  describe('when provided with a duplex stream', function() {
+    it('should return a duplex stream', function(done) {
+      // Simple pass-through duplex stream
+      const task = Task(null, () => through())
+
+      assert.isOk(isDuplex(task))
+
+      task.write('datum')
+      task.end()
+
+      task
+        .on('data', noop)
+        //.on('end', () => done())
+        .on('finish', () => done())
     })
   })
 })
