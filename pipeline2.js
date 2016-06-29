c39ecd1a63ec272469fa7c4ecd72a460507b459f
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-06-29 14:26:13 -0400

    Basic lifecycle logging

--- pipeline2.js
+++ pipeline2.js
@@ -10,16 +10,20 @@ const { shell, shellPipe } = waterwheel.wrappers
 
 const THREADS = 4
 const config = {
+  name: 'Salmonella enterica',
   sraAccession: '2492428',
   referenceURL: 'http://ftp.ncbi.nlm.nih.gov/genomes/all/GCA_000988525.2_ASM98852v2/GCA_000988525.2_ASM98852v2_genomic.fna.gz'
 }
 
 const downloadReference = Task({
   input: config.referenceURL,
-  output: new File(config.referenceURL.split('/').pop())
+  output: new File(config.referenceURL.split('/').pop()),
+  name: `Download reference genome for ${config.name}` 
 }, ({ input, output }) => request(input).pipe(fs.createWriteStream(output.value)) )
 
 const bwaIndex = Task({
   input: new File('*_genomic.fna.gz'),
   output: ['amb', 'ann', 'bwt', 'pac', 'sa'].map(suffix => new File(`*_genomic.fna.gz.${suffix}`))
 }, ({ input }) => shell(`bwa index ${input}`) )
+
+downloadReference()
