579fed87de6a814ad7f88e1e69c2ca906755b5c8
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-06 15:41:46 -0400

    Simple joins test, closes #11

--- test/Task.spec.js
+++ test/Task.spec.js
@@ -138,37 +138,55 @@ describe('Task', function() {
 })
 
 describe('Join', function() {
-  it.skip('should join two tasks', function(done) {
+  it('should join two tasks with stream, stream', function(done) {
     const task1 = Task({
       input: { value: ['1\n', '2\n'] },
       output: { file: 'foo.txt' },
       name: 'Write numbers to file'
-    }, ({ input }) => intoStream(input).pipe(fs.createWriteStream('foo.txt')) )
+    }, ({ input }) =>
+      intoStream(input)
+        .pipe(fs.createWriteStream('foo.txt'))
+    )
 
     const task2 = Task({
       input: { file: 'foo.txt' },
       output: { file: 'sum.txt' },
       name: 'Sum numbers in file'
-    }, ({ input }) => new Promise((resolve, reject) => {
-      resolve(42)
-
-      let sum = 0
-
+    }, ({ input }) =>
       fs.createReadStream(input)
         .pipe(split())
-        .on('data', function(line) {
-          if (line !== '') {
-            sum += parseInt(line)
-          } else {
-            resolve(sum)
+        .pipe(through(function(chunk, enc, done) {
+          if (!this._sum) {
+            this._sum = 0
           }
-        })
-    }))
 
-    // task2.on('data', noop)
+          const line = chunk.toString()
+
+          if (line !== '') {
+            this._sum += parseInt(line)
+          } else {
+            this.push('' + this._sum)
+          }
+
+          done()
+        }))
+        .pipe(fs.createWriteStream('sum.txt'))
+    )
 
     join(task1, task2)()
-      .on('data', (data) => console.log('data: ', data.toString()))
-      .on('end', () => console.log('end'))
+      .on('task.done', done)
+      .on('error', done)  
+  })
+
+  it.skip('should join two tasks with promise, promise', function(done) {
+
+  })
+
+  it.skip('should join two tasks with promise, stream', function(done) {
+    
+  })
+
+  it.skip('should join two tasks with stream, promise', function(done) {
+
   })
 })
