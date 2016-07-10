72c7351409faac1590c153e970a6ca40b717c82c
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-10 11:40:12 -0400

    parallel can parallel parallel, closes #13

--- test/parallel.spec.js
+++ test/parallel.spec.js
@@ -5,15 +5,17 @@ const { assert } = require('chai')
 const task = require('../lib/Task.js')
 const parallel = require('../lib/parallel.js')
 
+
+const delayedResolve = (str, time) => task({
+  name: `Return ${str} after ${time}ms`
+}, () => new Promise((resolve, reject) => {
+  setTimeout(() => resolve(str), time)
+}))
+
 describe('Parallel', function() {
   it('should run two tasks in parallel', function(done) {
-    const task1 = task({ name: 'parallel 1' }, () => new Promise((resolve, reject) => {
-      setTimeout(() => resolve('foo'), 1000)
-    }))
-
-    const task2 = task({ name: 'parallel 2' }, () => new Promise((resolve, reject) => {
-      setTimeout(() => resolve('bar'), 500)
-    }))
+    const task1 = delayedResolve('foo', 1000)
+    const task2 = delayedResolve('bar', 500)
 
     parallel(task1, task2)()
       .on('close', function() {
@@ -22,6 +24,47 @@ describe('Parallel', function() {
         assert(data[0].toString() === 'bar')
         assert(data[1].toString() === 'foo') 
         
+        done()
+      })
+  })
+
+  it('should parallel parallel', function(done) {
+    const taskA1 = delayedResolve('A1', 100)
+    const taskA2 = delayedResolve('A2', 500)
+    const taskB1 = delayedResolve('B1', 200)
+    const taskB2 = delayedResolve('B2', 400)
+
+    const p1 = parallel(taskA1, taskA2)
+    const p2 = parallel(taskB1, taskB2)
+
+    parallel(p1, p2)()
+      .on('close', function() {
+        const data = this.output()
+
+        assert(data[0][0].toString() === 'B1')
+        assert(data[0][1].toString() === 'B2')
+        assert(data[1][0].toString() === 'A1')
+        assert(data[1][1].toString() === 'A2')
+
+        done()
+      })
+  })
+
+  it('should arrive in proper order', function(done) {
+    const taskA1 = delayedResolve('A1', 100)
+    const taskA2 = delayedResolve('A2', 500)
+    const taskB1 = delayedResolve('B1', 200)
+    const taskB2 = delayedResolve('B2', 400)
+
+    parallel(taskA1, taskA2, taskB1, taskB2)()
+      .on('close', function() {
+        const data = this.output()
+
+        assert(data[0].toString() === 'A1')
+        assert(data[1].toString() === 'B1')
+        assert(data[2].toString() === 'B2')
+        assert(data[3].toString() === 'A2')
+
         done()
       })
   })
