b1aef84d7496925117040327c200e983eacce1b3
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-10 11:09:12 -0400

    Can join joins, see #13

--- test/join.spec.js
+++ test/join.spec.js
@@ -10,46 +10,47 @@ const split = require('split')
 const Task = require('../lib/Task.js')
 const join = require('../lib/Join.js')
 
+const writeNumbers = (file) => Task({
+  input: { value: ['1\n', '2\n'] },
+  output: { file },
+  name: 'Write numbers to file',
+  skippable: false
+}, ({ input }) =>
+  intoStream(input)
+    .pipe(fs.createWriteStream(file))
+)
+
+const sumNumbers = (numbers, sum) => Task({
+  input: { file: numbers },
+  output: { file: sum },
+  name: 'Sum numbers in file',
+  skippable: false
+}, ({ input }) =>
+  fs.createReadStream(input)
+    .pipe(split())
+    .pipe(through(function(chunk, enc, done) {
+      if (!this._sum) {
+        this._sum = 0
+      }
+
+      const line = chunk.toString()
+
+      if (line !== '') {
+        this._sum += parseInt(line)
+      } else {
+        this.push('' + this._sum)
+      }
+
+      done()
+    }))
+    .pipe(fs.createWriteStream(sum))
+)
 
 describe('Join', function() {
   it('should join two tasks with stream, stream', function(done) {
-    const task1 = Task({
-      input: { value: ['1\n', '2\n'] },
-      output: { file: 'foo.txt' },
-      name: 'Write numbers to file'
-    }, ({ input }) =>
-      intoStream(input)
-        .pipe(fs.createWriteStream('foo.txt'))
-    )
-
-    const task2 = Task({
-      input: { file: 'foo.txt' },
-      output: { file: 'sum.txt' },
-      name: 'Sum numbers in file'
-    }, ({ input }) =>
-      fs.createReadStream(input)
-        .pipe(split())
-        .pipe(through(function(chunk, enc, done) {
-          if (!this._sum) {
-            this._sum = 0
-          }
-
-          const line = chunk.toString()
-
-          if (line !== '') {
-            this._sum += parseInt(line)
-          } else {
-            this.push('' + this._sum)
-          }
-
-          done()
-        }))
-        .pipe(fs.createWriteStream('sum.txt'))
-    )
-
-    join(task1, task2)()
+    join(writeNumbers('numbers.1.txt'), sumNumbers('numbers.1.txt', 'sum.1.txt'))()
       .on('close', done)
-      .on('error', done)  
+      .on('error', done)
   })
 
   it.skip('should join two tasks with promise, promise', function(done) {
@@ -62,5 +63,14 @@ describe('Join', function() {
 
   it.skip('should join two tasks with stream, promise', function(done) {
 
+  })
+
+  it('should join joins', function(done) {
+    const task1 = join(writeNumbers('numbers.2.txt'))
+    const task2 = join(sumNumbers('numbers.2.txt', 'sum.2.txt'))
+
+    join(task1, task2)()
+      .on('close', done)
+      .on('error', done)
   })
 })
