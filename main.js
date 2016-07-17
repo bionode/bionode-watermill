851f8dbd082827470806e5039ecfff186c879f38
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-17 17:45:13 -0400

    messily adds new task to tasks after making uid

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 'use strict'
 
-const { store } = require('.')
+const { task } = require('.')
 
-const actions = require('./lib/get-actions.js')(store.dispatch)
+// const actions = require('./lib/get-actions.js')(store.dispatch)
+//
+// actions.setConfigValue('resume', 'on')
+// actions.addTask({
+//   name: 'foo'
+// })
+// actions.addTask({
+//   name: 'bar',
+//   params: { extreme: true }
+// })
 
-actions.setConfigValue('resume', 'on')
-actions.addTask('foo')
-actions.addTask('bar')
+const intoStream = require('into-stream')
+const fs = require('fs')
+
+const myTask = task({
+  input: { value: 'foo' },
+  output: { file: 'file.txt' },
+  name: 'My Task'
+}, ({ input }) => intoStream(input).pipe(fs.createWriteStream('file.txt')))
+
+myTask()
+  .on('close' , function () {
+    this.output()
+  })
