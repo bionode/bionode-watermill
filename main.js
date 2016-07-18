6aa4fd7bea72847e4c9829a4a3b6b4ab628a840a
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-18 01:21:56 -0400

    runs and reruns simple task with redux (no shell)

--- main.js
+++ main.js
@@ -2,20 +2,8 @@
 
 const { task } = require('.')
 
-// const actions = require('./lib/get-actions.js')(store.dispatch)
-//
-// actions.setConfigValue('resume', 'on')
-// actions.addTask({
-//   name: 'foo'
-// })
-// actions.addTask({
-//   name: 'bar',
-//   params: { extreme: true }
-// })
-
 const intoStream = require('into-stream')
 const fs = require('fs')
-
 const myTask = task({
   input: { value: 'foo' },
   output: { file: 'file.txt' },
@@ -23,6 +11,14 @@ const myTask = task({
 }, ({ input }) => intoStream(input).pipe(fs.createWriteStream('file.txt')))
 
 myTask()
+  .on('bunja', () => console.log('got bunja'))
+  .on('finish', () => console.log('externally received finish'))
+  .on('wow', (uid) => console.log('got wow', uid))
   .on('close' , function () {
-    this.output()
+    console.log('============== GOT CLOSE =======================')
+    // creating output on close
+    // this.output()
+
+    // using already created output (preferred, but child process throws close itself)
+    console.log('output:', this._output)
   })
