071a118d27642b8e357b344e423cf1a357d4602f
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-26 13:37:27 -0400

    rework runValidators to use applicator

--- test/is-non-null.spec.js
+++ test/is-non-null.spec.js
@@ -14,6 +14,7 @@ const twd = path.resolve(__dirname, 'files', 'is-non-null')
 const isNonNull = require('../lib/validators/is-non-null.js')
 
 describe('is-non-null', function() {
+  // Passes locally but fails on Travis
   it('Should resolve if file has contents', () =>
     isNonNull(path.resolve(twd, 'non-empty.txt')).should.be.fulfilled
   )
