65247eccb84f6caff56698f40f1dd55c9e55d883
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-28 12:02:48 -0400

    Task returns event emitter, task.finish event

--- examples/capitalize/capitalize.js
+++ examples/capitalize/capitalize.js
@@ -21,11 +21,6 @@ const myTask = task({
 )
 
 myTask()
-  .on('close' , function () {
-    console.log('============== GOT CLOSE =======================')
-    // creating output on close
-    // this.output()
-
-    // using already created output (preferred, but child process throws close itself)
-    console.log('output:', this._output)
+  .on('task.finish' , function (task) {
+    console.log('output:', task.resolvedOutput)
   })
