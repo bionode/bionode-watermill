674455ad436af4efe3f520b4544c4628c261e71a
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-08-21 12:24:54 -0400

    Fix junction tests

--- test/junction.spec.js
+++ test/junction.spec.js
@@ -2,16 +2,19 @@
 
 const { assert } = require('chai')
 const fs = require('fs')
+const path = require('path')
+
+const twd = path.resolve(__dirname, 'files', 'junction')
 
 const { task, join, junction } = require('../')
 
 describe('junction', function() {
   it('join(junction(A, B), C) should work', function(done) {
     const simpleFileMaker = (content, ext) => task({
-      output: `*.${ext}`,
+      output: `${twd}/*.${ext}`,
       name: 'Simple file maker with ' + content
     }, ({ input }) => {
-      const ws = fs.createWriteStream(content + '.' + ext)
+      const ws = fs.createWriteStream(twd + '/' + content + '.' + ext)
       ws.write(content)
       ws.end()
 
@@ -20,12 +23,13 @@ describe('junction', function() {
 
     const concatFiles = task({
       input: {
-        foo: '*.foo',
-        bar: '*.bar'
+        foo: `${twd}/*.foo`,
+        bar: `${twd}/*.bar`
       },
-      output: '*.foobar'
+      name: 'Concat files',
+      output: '${twd}/*.foobar'
     }, ({ input }) => {
-      const ws = fs.createWriteStream('concat.foobar')
+      const ws = fs.createWriteStream(twd + '/' + 'concat.foobar')
       const rsFoo = fs.createReadStream(input.foo)
       const rsBar = fs.createReadStream(input.bar)
 
@@ -50,7 +54,7 @@ describe('junction', function() {
 
     pipeline().then((results) => {
       console.log('pipeline: ', results)
-      fs.readFile('concat.foobar', (err, data) => {
+      fs.readFile(twd + '/' + 'concat.foobar', (err, data) => {
         if (err) return done(err)
         assert.equal(data, 'foobar')
         done()
