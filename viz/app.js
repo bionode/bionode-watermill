ed6946bd28bea9f1ca91af84c8a75226555efe1e
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-07-03 13:07:27 +0100

    Added a first implementation of real-time graph viz

--- viz/app.js
+++ viz/app.js
@@ -6,21 +6,22 @@ const fs = require('fs')
 // Loading the index file . html displayed to the client
 
 const server = http.createServer(function(req, res) {
-  fs.readFile('index.html', 'utf-8', function(error, content) {
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+  fs.readFile('./index.html', 'utf-8', function(error, content) {
+    res.writeHead(200, {"Content-Type": "text/html"})
     res.end(content)
   })
 })
 
 // Loading socket.io
 
-const io = require('socket.io').listen(server)
+const io = require('socket.io').listen(8084)
 // When a client connects, we note it in the console
 io.sockets.on('connection', function (socket) {
   console.log('A client is connected!')
-});
-server.listen(8084)
+})
+
+//server.listen(8084)
 
 // End of setuping server
 
-module.exports = server
+module.exports = io
