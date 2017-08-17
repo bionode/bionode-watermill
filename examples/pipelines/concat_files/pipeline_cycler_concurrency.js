eae734a1bfb9337bf9219c7fea6491479571f70b
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-08-17 10:06:28 +0100

    added a comment on test pipeline for concurrency

--- examples/pipelines/concat_files/pipeline_cycler_concurrency.js
+++ examples/pipelines/concat_files/pipeline_cycler_concurrency.js
@@ -10,10 +10,19 @@ const {
 
 const fs = require('fs')
 const path = require('path')
+const Promise = require('bluebird')
+
+// makes concurrency the third argument of code in shell
+const concurrency = parseFloat(process.argv[2] || "Infinity")
+
+/**************************/
+/* PIPELINE FOR EACH FILE */
+/**************************/
 
 // wrap every task and pipeline of a file in a single function (in this case
 // 'pipeline' function
 const fileFlow = (infile) => {
+  console.log('inputfile', infile)
   const concatTask = task({
     // notice that the input file (infile) is here passed as a param of the
     // pipeline function
@@ -38,19 +47,26 @@ const fileFlow = (infile) => {
   return pipelineAssemblage
 }
 
+/************************************************/
+/* CONTROL THE FLOW OF INPUTS INTO THE PIPELINE */
+/************************************************/
+
 // checks for files in current working directory (where the script is executed)
 fs.readdir(process.cwd(), (err, files) => {
+  const desiredFiles = []
   files.forEach(infile => {
-    // checks file extension, in this case '.fas'
+    // checks file extension, in this case '.fas' and creates an array with
+    // just these files
     if (path.extname(infile) === '.fas') {
-      // executes the pipeline function for each infile
-      const pipelineMaster = fileFlow(infile)
-      pipelineMaster()
-      // this is nice because in fact each pipelineMaster creates its own DAG
-      // however graphson output file may be corrupt because DAGs are being
-      // fired for each file, so last one to get set is the one saved to
-      // graphson.json
-      // TODO fix this graphson output file behavior
+      desiredFiles.push(infile)
     }
   })
+  // bluebird Promise.map is used here for concurrency where we can
+  // limit the flow of files into the pipeline. So, if concurrency is set
+  // to 1 it will first execute the first file, then the second and so on.
+  // But if 2 it will execute the first 2 files and then the other 2.
+  return Promise.map(desiredFiles, (infile) => {
+    const pipelineMaster = fileFlow(infile)
+    return pipelineMaster()
+  }, {concurrency})
 })
