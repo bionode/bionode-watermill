297945e34e1712d5d6d376ba5c0f4e836e4e0e9c
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-08-21 06:55:57 +0100

    added a test for nested forks wrapped in joins

--- test/fork_fork.spec.js
+++ test/fork_fork.spec.js
@@ -3,6 +3,10 @@
 const { task, fork, join, junction } = require('../')
 
 const { assert } = require('chai')
+const fs = require('fs');
+const obj = JSON.parse(fs.readFileSync('./graphson.json', 'utf8'));
+
+// Define tasks
 
 const task0 = task({name: 'task0_2"'}, () => `echo "something0_2"`)
 
@@ -19,7 +23,7 @@ const task5 = task({name: 'task5_2'}, () => `echo "something5_2"`)
 const task6 = task({name: 'task6_2'}, () => `echo "something6_2"`)
 
 describe('fork_fork', () => {
-  it('fork should work after a fork, wrapped in a join', () => {
+  it('fork should work after a fork, wrapped in a join', (done) => {
     const pipeline = join(
       task0,
       fork(
@@ -40,7 +44,10 @@ describe('fork_fork', () => {
 
       // should assure that both ends of the pipeline exist
       assert.equal(results[1].tasks[1], results[1].context.trajectory[2])
+      //checks if pipeline rendered the right number of vertices and edges
+      assert.equal(obj.graph.vertices.length, 10)
+      assert.equal(obj.graph.edges.length, 9)
       //done() // without done it is not really testing anything
     })
-  })
+  }).timeout(5000)
 })
