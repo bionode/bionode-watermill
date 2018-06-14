0379a23521effe217a293cd0a4c9bcb5dd428c54
evoxtorm <evostorm96@gmail.com>
2018-06-15 02:08:39 +0530

    Hardcoded relative to the current directory

--- test/workflow-status/testpipeline.js
+++ test/workflow-status/testpipeline.js
@@ -6,7 +6,7 @@ const { task, join } = require('../..')
 const anotherTask = task({
   name: 'Run a JS file',
   output: 'ran.txt'
-}, () => `node wait-error.js && touch ran.txt`)
+}, () => `node /home/evoxtorm/Desktop/Bionode-watermill/bionode-watermill/test/workflow-status/wait-error.js && touch ran.txt`)
 
 anotherTask()
 
