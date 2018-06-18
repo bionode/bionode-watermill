f8fba790cc19a049d4a2296911c30f47dfbf07b9
evoxtorm <evostorm96@gmail.com>
2018-06-18 09:43:26 +0530

    Unit testing and removed typo mistake

--- test/workflow-status/testpipeline.js
+++ test/workflow-status/testpipeline.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const { task, join } = require('../..')
+const { assert } = require('chai')
+const mocha = require('mocha')
+
+const { store } = require('../..')
+
 
 
 const anotherTask = task({
@@ -8,7 +13,34 @@ const anotherTask = task({
   output: 'ran.txt'
 }, () => `node /home/evoxtorm/Desktop/Bionode-watermill/bionode-watermill/test/workflow-status/wait-error.js && touch ran.txt`)
 
-anotherTask()
+//anotherTask()
+
+// Testing the work-flow Status
+
+describe('Failed pipeline', function () {
+  it('should have permanentFailure', (done) =>
+    anotherTask()
+     .then(() => done(new Error('Should not have succeeded'))
+    .catch((err) => {
+       try {
+          expect(store.getState().workflowState).to.equal('PERMANENT_FAILURE')
+          done()
+     } catch (err) {
+        done(err)
+     }
+   })
+)
+).timeout(10000)
+})
+
+
+	// .then(console.log)
+	// .catch(console.error)
 
 
 
+	
+	
+
+
+	
