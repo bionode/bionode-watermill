9e0fe53d58431486eaa0f38d510af8799f950282
evoxtorm <evostorm96@gmail.com>
2018-06-22 03:15:21 +0530

    Path resolved

--- test/workflow-status/testpipeline.js
+++ test/workflow-status/testpipeline.js
@@ -8,6 +8,7 @@ const {
   assert
 } = require("chai");
 const mocha = require("mocha")
+const path = require('path')
 
 const {
   store
@@ -19,12 +20,13 @@ const anotherTask = task({
     output: "ran.txt"
   },
   () =>
-  `node /home/evoxtorm/Desktop/Bionode-watermill/bionode-watermill/test/workflow-status/wait-error.js && touch ran.txt`
+  `node ${path.resolve(__dirname, 'wait-error.js')} && touch ran.txt`
 )
 
-//anotherTask()
 
-// Testing the work-flow Status
+anotherTask()
+
+Testing the work-flow Status
 
 describe("Failed pipeline", function() {
   it("Should have permanentFailure", done =>
